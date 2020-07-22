a1535d2cf1be51186a116961429a9e0d498ebf54
minnahei <minna.s.heinonen@gmail.com>
2020-07-22 19:56:50 +0200

    Inital commit

---
+++ Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using leave_management.Data;
+
+namespace leave_management.Contracts
+{
+    public interface ILeaveAllocationRepository : IRepositoryBase<LeaveAllocation>
+    {
+        Task<bool> CheckAllocation(int leavetypeid, string employeeid);
+        Task<ICollection<LeaveAllocation>> GetLeaveAllocationsByEmployee(string employeeid);
+        Task<LeaveAllocation> GetLeaveAllocationsByEmployeeAndType(string employeeid, int leavetypeid);
+    }
+}
---
+++ Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using leave_management.Contracts;
+using leave_management.Data;
+
+namespace leave_management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Task<bool> CheckAllocation(int leavetypeid, string employeeid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<LeaveAllocation>> GetLeaveAllocationsByEmployee(string employeeid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveAllocation> GetLeaveAllocationsByEmployeeAndType(string employeeid, int leavetypeid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
