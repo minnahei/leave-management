0029dd01c91429d1701911ecf595730c5251503c
minnahei <minna.s.heinonen@gmail.com>
2020-07-30 13:03:55 +0200

    Setup user Auth, roles and admin. Modified registration process

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -1,5 +1,5 @@
 using System;
-using leave_management.Areas.Identity.Data;
+using leave_management.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -15,12 +15,6 @@ namespace leave_management.Areas.Identity
         public void Configure(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) => {
-                services.AddDbContext<IdentityDataContext>(options =>
-                    options.UseSqlServer(
-                        context.Configuration.GetConnectionString("IdentityDataContextConnection")));
-
-                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddEntityFrameworkStores<IdentityDataContext>();
             });
         }
     }
---
+++ SeedData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace leave_management
+{
+    public static class SeedData //static, no need to instantiate objects, everythign have to be static
+    {
+        public static void Seed(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+
+        }
+
+        private static void SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if(userManager.FindByNameAsync("admin@localhost.com").Result == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = "admin@localhost.com",
+                    Email = "admin@localhost.com"
+                };
+                var result = userManager.CreateAsync(user, "P@ssword1").Result;
+
+                if(result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait(); // We created the user but we need to assign to a role
+                }
+            }
+
+        }
+
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if(!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Administrator"
+                };
+
+                var result = roleManager.CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Employee"
+                };
+
+                var result = roleManager.CreateAsync(role).Result;
+            }
+        }
+    }
+}
